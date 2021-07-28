cdae0577f1054aa128eb4c179ffee733e232a835
aino-shubham12 <shubham2020@ainosoft.com>
2021-07-28 16:35:37 +0530

    WIP : added copyright license to java source files

--- src/main/java/intern/demo/MenuItem.java
+++ src/main/java/intern/demo/MenuItem.java
@@ -1,3 +1,27 @@
+/*
+ * AppOps is a Java framework to develop, deploy microservices with ease and is available for free
+ * and common use developed by AinoSoft.
+ * 
+ * AppOps and AinoSoft are registered trademarks of Aino Softwares private limited
+ * 
+ * Copyright (C) <2016> <Aino Softwares private limited>
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version along with applicable additional terms as
+ * provisioned by GPL 3.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License and applicable additional terms
+ * along with this program.
+ * 
+ * If not, see <https://www.gnu.org/licenses/> <https://www.appops.org/license>
+ */ 
+ 
+ 
 package intern.demo;
 
 import intern.demo.action.Action;
