a1f7d9ae6532b0835cb8d2f07b04ccacdf274cfa
aino-shubham12 <shubham2020@ainosoft.com>
2021-08-10 12:54:09 +0530

    WIP : to add,remove license header mycila maven plugin added.

--- src/main/java/intern/demo/action/ExitAddressAction.java
+++ src/main/java/intern/demo/action/ExitAddressAction.java
@@ -1,27 +1,27 @@
 /*
  * AppOps is a Java framework to develop, deploy microservices with ease and is available for free
  * and common use developed by AinoSoft ( www.ainosoft.com )
- * 
+ *
  * AppOps and AinoSoft are registered trademarks of Aino Softwares private limited, India.
- * 
- * Copyright (C) <2016> <Aino Softwares private limited>
- * 
+ *
+ * Copyright © 2016 <Aino Softwares private limited>
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version along with applicable additional terms as
  * provisioned by GPL 3.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License and applicable additional terms
  * along with this program.
- * 
+ *
  * If not, see <https://www.gnu.org/licenses/> and <https://www.appops.org/license>
+ *
+ *
  */
- 
- 
 package intern.demo.action;
 
 import java.util.ArrayList;
